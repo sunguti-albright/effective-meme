3c0402f672bf4a254b50ef2246db5da4565f040a
AlbrightSunguti <sungutialbright@gmail.com>
2023-04-20 16:59:37 +0300

    chore: register courses repository in course module providers

--- rest-api/src/courses/courses.module.ts
+++ rest-api/src/courses/courses.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
+import { CoursesRepository } from './repositories/courses.repository';
 
 @Module({
-    controllers:[CoursesController]
+    controllers:[CoursesController],
+    providers:[CoursesRepository]
 })
 export class CoursesModule {}
