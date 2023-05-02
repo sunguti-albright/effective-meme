7fcf5d950e7e17f2d04d5486678fd81d08a59a69
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-02 17:29:35 +0300

    add course controller

--- rest-api/src/courses/courses.controller.ts
+++ rest-api/src/courses/courses.controller.ts
@@ -1,17 +1,20 @@
-import { Body, Controller, Delete, Get, Param, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { Course } from 'src/shared/course';
 import { CoursesRepository } from './repositories/courses.repository';
 
 //controllers send requests to the server and return the specific responses
 @Controller('courses')
 export class CoursesController {
-  constructor(private courseRepo: CoursesRepository) {}
+  constructor(private courseRepo: CoursesRepository) { }
 
   // @Get('api/hello')
   // async helloWorld() {
   //   return 'hello';
   // }
-
+  @Post()
+  async createCourse(@Body() course: Partial<Course>): Promise<Course> {
+    return this.courseRepo.createCourse(course)
+  }
   @Get()
   async getAllCourses(): Promise<Course[]> {
     //get data from mongo-db
@@ -30,7 +33,7 @@ export class CoursesController {
   @Delete(':courseId')
   async deleteCourse(
     @Param('courseId') courseId: string
-  ): Promise<any>{
+  ): Promise<any> {
     return this.courseRepo.deleteOne(courseId)
   }
 }
