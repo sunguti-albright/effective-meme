9319769d62915a19496cb4f3b229be73063e5796
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-02 16:19:10 +0300

    chore: update course with course id param, and body(partial)

--- rest-api/src/courses/courses.controller.ts
+++ rest-api/src/courses/courses.controller.ts
@@ -1,22 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put } from '@nestjs/common';
 import { Course } from 'src/shared/course';
 import { CoursesRepository } from './repositories/courses.repository';
-
 
 //controllers send requests to the server and return the specific responses
 @Controller()
 export class CoursesController {
-  constructor(private courseRepo : CoursesRepository){}
-  
+  constructor(private courseRepo: CoursesRepository) {}
+
+  @Get('api/hello')
+  async helloWorld() {
+    return 'hello';
+  }
+
   @Get('api/courses')
   async getAllCourses(): Promise<Course[]> {
     //get data from mongo-db
-    return this.courseRepo.findAll()
-    // return findAllCourses();
+    return this.courseRepo.findAll();
+    // return findAllCourses(); -----this works locally
   }
 
-  @Get('api/hello')
-  async helloWorld(){
-    return "hello"
-  }
+  @Put('api/courses/:courseId')
+  async updateCourse(
+    @Param('courseId') courseId: string,
+    @Body() changes: Partial<Course>,
+  ) {}
 }
