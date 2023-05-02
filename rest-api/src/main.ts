70f05191239e46b5a443b4d077a0fd9c48201288
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-02 16:33:26 +0300

    set backend global prefix

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -5,7 +5,7 @@ declare const module : any;
 async function bootstrap(){
 const app = await NestFactory.create(AppModule);
 app.listen(9000);
-
+app.setGlobalPrefix("api");
 if(module.hot){
 module.hot.accept();
 module.hot.dispose(()=>app.close());
