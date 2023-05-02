b175e6442c82d447cb8d349ec9f82815cc82a1ea
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-02 12:03:28 +0300

    register mongoose module in app module

--- rest-api/src/app.module.ts
+++ rest-api/src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CoursesModule } from './courses/courses.module';
+import {MongooseModule} from '@nestjs/mongoose';
+import { CONNECTION_STRING } from './constants';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, 
+    MongooseModule.forRoot(CONNECTION_STRING),],
   controllers: [],
   exports: [],
   providers: [],
