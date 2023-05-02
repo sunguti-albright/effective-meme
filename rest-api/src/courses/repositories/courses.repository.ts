1a36c722cf9aeffe7882ee3e4c4d7a1068455c89
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-02 14:43:57 +0300

    courses repository for making requests to mongo db server

--- rest-api/src/courses/repositories/courses.repository.ts
+++ rest-api/src/courses/repositories/courses.repository.ts
@@ -1,8 +1,29 @@
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Course } from "src/shared/course";
 
 
 
 @Injectable()
 export class CoursesRepository{
-    
+
+    constructor(@InjectModel('Course')private courseModel : Model<Course>){}
+
+   async findOne() : Promise<Course[]>{
+        return
+    }
+
+    async findAll() : Promise<Course[]>{
+        return this.courseModel.find();
+    }
+
+    async updateOne() : Promise<Course[]>{
+        return
+    }
+
+    async deleteOne() : Promise<Course[]>{
+        return
+    }
+
 }
