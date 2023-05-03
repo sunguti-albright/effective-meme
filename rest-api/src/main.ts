b64b73cf4681462b35eeaf1d27f68c2206b288a8
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-03 09:53:17 +0300

    register app to use validations

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 declare const module : any;
 async function bootstrap(){
 const app = await NestFactory.create(AppModule);
-app.listen(9000);
+//register validations
+app.useGlobalPipes(new ValidationPipe())
+//app runs on this port
+await app.listen(9000);
+//set global prefix for apis
 app.setGlobalPrefix("api");
+//hot module reload
 if(module.hot){
 module.hot.accept();
 module.hot.dispose(()=>app.close());
