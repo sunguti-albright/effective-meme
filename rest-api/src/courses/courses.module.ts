51cb3b3e5c52f012bfac8b5860bfd4546d6c0211
AlbrightSunguti <sungutialbright@gmail.com>
2023-05-03 10:43:35 +0300

    fix/change course schema

--- rest-api/src/courses/courses.module.ts
+++ rest-api/src/courses/courses.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { CoursesController } from './courses.controller';
 import { CoursesRepository } from './repositories/courses.repository';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CoursesSchema } from './schemas/courses.schema';
 
 @Module({
     imports:[
         //define entity and mongoose schema for module
         MongooseModule.forFeature([
             {
-                name: "Course", schema: "CoursesSchema"
+                name: "Course", schema: CoursesSchema
             }
         ])
     ],
